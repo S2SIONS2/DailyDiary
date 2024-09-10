ac81979a2c9c04b58311b8e62b36ef85cc5ea6ad
Sion <solee9802@gmail.com>
2024-09-10 21:18:26 +0900

    초기 커밋

---
+++ DailyDiary/src/components/Button.tsx
@@ -0,0 +1,11 @@
+import './Button.scss'
+
+const Button = () => {
+    return (
+        <div className='Button'>
+            <button type='button'>버튼</button>
+        </div>
+    )
+}
+
+export default Button;
