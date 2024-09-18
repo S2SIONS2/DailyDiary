792fc7156b5c2f20a88081da5bf1521356ab6464
Sion <solee9802@gmail.com>
2024-09-18 23:52:35 +0900

    독서록 페이지 추가

---
+++ DailyDiary/src/pages/BookDiary.tsx
@@ -0,0 +1,11 @@
+import './BookDiary.scss'
+
+const BookDiary = () => {
+    return (
+        <div className="BookDiary">
+            BookDiary
+        </div>
+    )
+}
+
+export default BookDiary;
