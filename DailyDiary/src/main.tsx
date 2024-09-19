9e074d7bd760fda01a8a871cd8de38d3c05b84eb
Sion <solee9802@gmail.com>
2024-09-19 23:59:20 +0900

    페이지 라우트 경로 추가(데이터 없음, 페이지 없음)

--- DailyDiary/src/main.tsx
+++ DailyDiary/src/main.tsx
@@ -1,8 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+
 import App from './App.tsx'
 import BookDiary from './pages/BookDiary'
-import './index.css'
+import NoData from './pages/NoData.tsx'
+import NotFound from './pages/NotFound.tsx'
 
 import { Provider } from 'react-redux';
 import { store } from './app/store.ts';
@@ -15,6 +18,8 @@ createRoot(document.getElementById('root')!).render(
           <Route index element={<BookDiary />} />
           <Route path='bookdiary' element={<BookDiary />} />
         </Route>
+        <Route path='/nodata' element={<NoData />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </Provider>
