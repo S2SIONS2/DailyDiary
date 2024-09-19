82e6d4f00b2f4a185cbe11af9c4861e3ef3acddd
Sion <solee9802@gmail.com>
2024-09-19 23:59:50 +0900

    페이지 추가(데이터 없음, 페이지 없음)

---
+++ DailyDiary/src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <div className="NotFound">
+            <div className="text-center mb-2">404 Not Found</div>
+            <div className="text-center">페이지가 없습니다.</div>
+        </div>
+    )
+}
+
+export default NotFound;
