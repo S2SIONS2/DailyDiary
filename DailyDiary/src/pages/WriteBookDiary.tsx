02af7747b9f077224f29a7da229de88b4f463a5d
Sion <solee9802@gmail.com>
2024-09-23 04:26:40 +0900

    book diary 리스트 추가 구현했으나, id값 랜덤 기입됨 수정 예정

--- DailyDiary/src/pages/WriteBookDiary.tsx
+++ DailyDiary/src/pages/WriteBookDiary.tsx
@@ -1,10 +1,18 @@
-import Loading from './Loading';
+import './WriteBookDiary.scss'
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, AppDispatch } from '../../src/app/store';
 import { fetchBooks } from '../features/api/BookSlice';
+import axios from 'axios'
+import Button from '../components/Button';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
 
 const WriteBookDiary: React.FC = () => {
+    const navigate = useNavigate();
     const bookList = useSelector((state: RootState) => state.books.bookList) // api list
     const status = useSelector((state: RootState) => state.books.status) // api list status
     const dispatch = useDispatch<AppDispatch>();
@@ -12,6 +20,7 @@ const WriteBookDiary: React.FC = () => {
     const [ bookTitle, setBookTitle ] =useState('') // 책 제목
     const [ author, setAuthor ] =useState('') // 책 저자
     const [ description, setDescription ] =useState('') // 책 줄거리
+    const [ bookImg, setBookImg] = useState('')
 
     // 책 제목 입력
     const handleBookTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,13 +36,23 @@ const WriteBookDiary: React.FC = () => {
     // 책 갯수 3개씩 페이징
     const [ currentPage, setCurrentPage ] = useState(1); // api start 위치
     const [ presentPage, setPresentPage] = useState(1) // 현재 리스트 페이지
+    const total = useSelector((state: RootState) => state.books.totalBookList) // 전체 리스트 길이
     const handlePreBtn = (pageNumber: number) => {
-        setCurrentPage(currentPage - pageNumber)
-        setPresentPage(presentPage - 1)
+        if( presentPage == 1 || currentPage == 1 ) {
+            setCurrentPage(1)
+            setPresentPage(1)
+        }else {
+            setCurrentPage(currentPage - pageNumber)
+            setPresentPage(presentPage - 1)
+        }
     } 
     const handleNextBtn = (pageNumber: number) => {
-        setCurrentPage(currentPage + pageNumber)
-        setPresentPage(presentPage + 1)
+        if ( currentPage + 3 >= total){
+            alert('마지막 리스트 입니다.')
+        }else {
+            setCurrentPage(currentPage + pageNumber)
+            setPresentPage(presentPage + 1)
+        }
     }    
 
     useEffect(() => {
@@ -48,70 +67,110 @@ const WriteBookDiary: React.FC = () => {
         setAuthor(e.target.value);
     };
 
-    // 책 줄거리 입력
+    // 독후감 내용 입력
     const handleDescription = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setDescription(e.target.value);
     };
 
-    // db.json에 코드 담기 
-    // await axios.post('http://localhost:3000/book', {
-    //     title: response.data.item[0].title,
-    //     image: response.data.item[0].image,
-    //     author: response.data.item[0].author,
-    //     isbn: response.data.item[0].isbn,
-    //     description: response.data.item[0].description
-    // });
+    // 선택한 api 정보 값 추출
+    type bookInfo = {
+        title: string,
+        author: string,
+        image: string
+    }
+    const saveItemInfo = (book: bookInfo) => {
+        setBookTitle(book.title)
+        setAuthor(book.author)
+        setBookImg (book.image)
+    } 
 
+    // db.json에 코드 담기 
+    const confirmBtn = async () => {
+        try{
+            const url = 'http://localhost:3000/book';
+            const params = {
+                title: bookTitle,
+                author: author,
+                image: bookImg,
+                description: description 
+            }
+            const response = await axios.post(url, params)
+            if(response.data.code === 200) {
+                navigate('/app/bookdiary')
+            }
+        }catch(error) {
+            console.error(error)
+        }
+    }
+    // 닫기 버튼 클릭 시
+    const cancleBtn = async () => {
+        navigate('/app/bookdiary')
+    }
 
     return (
-        <div className="WriteBookDiary">
-            <section className='row mb-3'>
+        <div className="WriteBookDiary p-2">
+            <section className='row m-0 mb-3'>
                 <h4>책 제목</h4>
+                <p className='text-muted'>책 검색 후 기록할 책을 클릭 하시면 더욱 간편하게 작성이 가능합니다.</p>
                 <input type='text' className='w-auto me-2 flex-grow-1' value={bookTitle} onChange={handleBookTitle}/>
                 <button className='w-auto' onClick={handleSearch}>Search</button>
             </section>
-            <ul className='row'>
-                {
-                    status === 'loading' && <Loading />
-                }
+            <ul className='row list-ul p-0 m-0'>
                 {
                     bookList.map((book) => (
-                        <li key={book.isbn} className='border border-1 p-2 row flex-nowrap cursor-pointer'>
-                            <img src={book.image} alt={book.title} style={{ width: '100px', height: 'auto' }} />
-                            <div className='row flex-grow-1'>
-                                <h5 className='mb-1 row align-items-center'>{book.title}</h5>
-                                <p className='mb-1 row align-items-center'>{book.author}</p>
+                        <li key={book.isbn} 
+                            className='border border-1 p-2 g-0 row flex-nowrap cursor-pointer booklist'
+                            onClick={() => saveItemInfo(book)}
+                        >                            
+                            <div className='imgArea m-0 p-0'>
+                                <img src={book.image} alt={book.title} />
+                            </div>
+                            <div className='row p-0 m-0'>
+                                <h5 className='mb-1 row align-items-center p-0 m-0'>{book.title}</h5>
+                                <p className='mb-1 row align-items-center p-0 m-0'>{book.author}</p>
                             </div>
                         </li>
                     ))
                 }
                 {
-                    bookList && (
-                        <div className='row align-items-center justify-content-center'>
-                            <button
+                    bookList.length != 0 && (
+                        <div className='row align-items-center justify-content-center mt-3 gap-2'>
+                            <Button 
+                                text={<FontAwesomeIcon icon={faChevronLeft} />}
+                                type={"button"}
                                 onClick={() => handlePreBtn(3)}
-                                className='w-auto m-0 g-0'
-                            >
-                                -
-                            </button>
-                            <p className='w-auto m-0 g-0'>{presentPage}</p>
-                            <button
+                            />
+                            <p className='w-auto m-0 p-0'>{presentPage}</p>
+                            -
+                            <p className='w-auto m-0 p-0 text-muted'>{total == 1 ? 1 : Math.floor(total / 3)}</p>
+                            <Button 
+                                text={<FontAwesomeIcon icon={faChevronRight} />}
+                                type={"button"}
                                 onClick={() => handleNextBtn(3)}
-                                className='w-auto m-0 g-0'
-                            >
-                                +
-                            </button>
+                            />
                         </div>
                     )
                 }
             </ul>
-            <section className='row mb-3'>
+            <section className='row m-0 mb-3'>
                 <h4>저자</h4>
-                <input type='text' className='w-auto me-2 flex-grow-1' value={author} onChange={handleAuthor}/>
+                <input type='text' className='w-auto flex-grow-1' value={author} onChange={handleAuthor}/>
             </section>
-            <section className='row mb-3'>
+            <section className='row m-0 mb-3'>
                 <h4>독후감</h4>
-                <textarea className='w-auto me-2 flex-grow-1' value={description} onChange={handleDescription}/>
+                <textarea className='w-auto flex-grow-1' value={description} onChange={handleDescription}/>
+            </section>
+            <section className='row align-items-center justify-content-center gap-2 m-0 mb-3'>
+                <Button
+                    text={'닫기'}
+                    onClick={() => cancleBtn()}
+                    type={'cancle'}
+                />
+                <Button
+                    text={'저장'}
+                    onClick={() => confirmBtn()}
+                    type={"confirm"}
+                />
             </section>
         </div>
     )
