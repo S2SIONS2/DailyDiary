16ec365a065b264030959a265795047b4c9aaddd
Sion <solee9802@gmail.com>
2024-09-30 01:37:11 +0900

    Revert "Revert "proxy 오류로 api 직접 호출로 변경""
    This reverts commit 12ebab1c1c6ea78d70a024d7fd3339c8eb674529.

--- DailyDiary/src/features/api/BookListSlice.ts
+++ DailyDiary/src/features/api/BookListSlice.ts
@@ -39,7 +39,8 @@ type updateBookList = {
 }
 
 // api 주소
-const url = '/api/bookList';
+// const url = '/api/bookList';
+const url = 'http://175.212.136.236:8081/book';
 
 // 비동기 액션 생성
 // api get
@@ -103,13 +104,13 @@ const listSlice = createSlice({
             .addCase(addList.fulfilled, (state, action) => { // list 추가
                 state.saveBookList.push(action.payload)
             })
-            .addCase(updateList.fulfilled, (state, action) => {
+            .addCase(updateList.fulfilled, (state, action) => { // list 수정
                 const index = state.saveBookList.findIndex((list) => list.id === action.payload.id);
                 if(index !== -1){
                     state.saveBookList[index] = action.payload
                 }
             })
-            .addCase(deleteBook.fulfilled, (state, action) => {
+            .addCase(deleteBook.fulfilled, (state, action) => { // list 삭제
                 state.saveBookList = state.saveBookList.filter((list) => list.id !== action.payload)
             })
     }
